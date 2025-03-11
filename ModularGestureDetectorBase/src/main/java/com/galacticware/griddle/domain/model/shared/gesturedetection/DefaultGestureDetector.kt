7abbf1d8fc63a5a42bd48c18c9136f4f610d6b26
MXHJohn314 <mxhjohnson4dba@gmail.com>
2025-03-11 11:35:13 -0600

    Adding support for Boomerang gestures.

--- ModularGestureDetectorBase/src/main/java/com/galacticware/griddle/domain/model/shared/gesturedetection/DefaultGestureDetector.kt
+++ ModularGestureDetectorBase/src/main/java/com/galacticware/griddle/domain/model/shared/gesturedetection/DefaultGestureDetector.kt
@@ -10,11 +10,10 @@ import kotlin.math.hypot
 import com.galacticware.griddle.domain.model.shared.RotationDirection as RotationDirection1
 
 /**
- * Warning: This gesture detector does not detect [GenericGestureType.BOOMERANG] or
- * [GenericGestureType.CIRCLE] gestures.
+ * Warning: This gesture detector does not detect [GenericGestureType.CIRCLE] gestures.
  */
 object DefaultGestureDetector : IGestureDetector {
-
+   private const val JITTER_REMOVAL_DISTANCE = 15
     override fun reset() {}
 
     override fun determineGesture(
@@ -22,24 +21,45 @@ object DefaultGestureDetector : IGestureDetector {
         minimumDragLength: Int,
         duration: Long,
         touchPoints: MutableList<Point>,
-        //    layoutCoordinates: LayoutCoordinates
     ): Triple<GenericGestureType, Direction?, RotationDirection1?> {
         if(touchPoints.isEmpty()) throw Exception()
-        val pair = touchPoints.last() to touchPoints.first()
-        val (diffX, diffY, distance) = pair.let { (b, a) ->
-            val diffX = b.x - a.x
-            val diffY = b.y - a.y
-            Triple(diffX, diffY, hypot(diffX, diffY))
+
+        val points = touchPoints.dropLast(1)
+            .zip(touchPoints.drop(1))
+            .map {( p1, p2) -> hypot(p1.x - p2.x, p1.y - p2.y) }
+            .withIndex()
+            .filter {
+                val isThisPairJittery = it.value <= JITTER_REMOVAL_DISTANCE
+                val isLastPair = it.index == touchPoints.size - 1
+                val hasAtLeastThreePairs = touchPoints.size > 3
+                !isThisPairJittery || !hasAtLeastThreePairs || isLastPair
+            }
+            .map { touchPoints[it.index] }
+        val triples = points.withIndex().map { (it, x) ->
+            val p2 = points[it]
+            val d = hypot(points.first().x - p2.x, points.first().y - p2.y)
+            Triple(it, x, d)
         }
-        if(touchPoints.size < 2 || distance < minimumDragLength) {
+
+        val distanceTraveled = triples.sumOf { it.third }
+
+        if(points.size < 2 || distanceTraveled < minimumDragLength) {
             return Triple(
                 if(duration > minimumHoldTime) GenericGestureType.HOLD else GenericGestureType.CLICK,
                 null,
                 null
             )
         }
+        val farPointTriple = triples.maxBy { it.third }
+        val (diffX, diffY) = (farPointTriple.second to points.first())
+            .let { (b, a) ->
+                val diffX = b.x - a.x
+                val diffY = b.y - a.y
+                diffX to diffY
+            }
+
         return Triple(
-            GenericGestureType.SWIPE,
+            if(farPointTriple.second != points.last() && triples.slice(0 until farPointTriple.first).sumOf { it.third } >=  .5 * triples.slice(farPointTriple.first until points.size).sumOf { it.third }) GenericGestureType.BOOMERANG else GenericGestureType.SWIPE,
             if(abs(diffX) > abs(diffY/2) && abs(diffY) > abs(diffX/2)) {
                 // diagonal
                 if(diffX > 0 && diffY > 0) {
